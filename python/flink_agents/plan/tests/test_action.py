f799c053b50c56bd5a72cc87c97f1061fe52a85b
Wenjin Xie <166717626+wenjin272@users.noreply.github.com>
2025-06-26 17:38:29 +0800

    [plan][python] Support serialize/deserialize workflow plan to/from json. (#39)

--- python/flink_agents/plan/tests/test_action.py
+++ python/flink_agents/plan/tests/test_action.py
@@ -15,6 +15,9 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 #################################################################################
+import json
+from pathlib import Path
+
 import pytest
 
 from flink_agents.api.event import InputEvent
@@ -33,7 +36,7 @@ def test_action_signature_legal() -> None: # noqa: D103
     Action(
         name="legal",
         exec=PythonFunction.from_callable(legal_signature),
-        listen_event_types=[InputEvent],
+        listen_event_types=[f'{InputEvent.__module__}.{InputEvent.__qualname__}'],
     )
 
 def test_action_signature_illegal() -> None:  # noqa: D103
@@ -41,7 +44,37 @@ def test_action_signature_illegal() -> None:  # noqa: D103
         Action(
             name="illegal",
             exec=PythonFunction.from_callable(illegal_signature),
-            listen_event_types=[InputEvent],
+            listen_event_types=[f'{InputEvent.__module__}.{InputEvent.__qualname__}'],
         )
+
+@pytest.fixture(scope="module")
+def action() -> Action: # noqa: D103
+    func = PythonFunction.from_callable(legal_signature)
+    return Action(
+        name="legal",
+        exec=func,
+        listen_event_types=[f'{InputEvent.__module__}.{InputEvent.__qualname__}'],
+    )
+
+current_dir = Path(__file__).parent
+
+def test_action_serialize(action: Action) -> None:  # noqa: D103
+    json_value = action.model_dump_json(serialize_as_any=True)
+    with Path.open(Path(f'{current_dir}/resources/action.json')) as f:
+        expected_json = f.read()
+    actual = json.loads(json_value)
+    expected = json.loads(expected_json)
+    assert actual == expected
+
+def test_action_deserialize(action: Action) -> None: # noqa: D103
+    with Path.open(Path(f'{current_dir}/resources/action.json')) as f:
+        expected_json = f.read()
+    action = Action.model_validate_json(expected_json)
+    assert action.name == 'legal'
+    assert action.listen_event_types == ['flink_agents.api.event.InputEvent']
+    func = action.exec
+    assert func.module == 'flink_agents.plan.tests.test_action'
+    assert func.qualname == 'legal_signature'
+
 
 
--- python/flink_agents/plan/workflow_plan.py
+++ python/flink_agents/plan/workflow_plan.py
@@ -15,12 +15,10 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 #################################################################################
-
-from typing import Dict, List, Type
+from typing import Dict, List
 
 from pydantic import BaseModel
 
-from flink_agents.api.event import Event
 from flink_agents.api.workflow import Workflow
 from flink_agents.plan.action import Action
 from flink_agents.plan.function import PythonFunction
@@ -31,26 +29,29 @@ class WorkflowPlan(BaseModel):
 
     Attributes:
     ----------
-    actions : Dict[Type[Event], List[Action]]
-        Mapping of event types to the list of Actions that listen to them.
+    actions: Dict[str, Action]
+        Mapping of action names to actions
+    event_trigger_actions : Dict[Type[Event], str]
+        Mapping of event types to the list of actions name that listen to them.
     """
-
-    actions: Dict[Type[Event], List[Action]]
+    actions: Dict[str, Action]
+    event_trigger_actions: Dict[str, List[str]]
 
     @staticmethod
     def from_workflow(workflow: Workflow) -> "WorkflowPlan":
         """Build a WorkflowPlan from user defined workflow."""
         actions = {}
+        event_trigger_actions = {}
         for action in _get_actions(workflow):
             assert action.name not in actions, f"Duplicate action name: {action.name}"
+            actions[action.name] = action
             for event_type in action.listen_event_types:
-                if event_type not in actions:
-                    actions[event_type] = []
-                actions[event_type].append(action)
-        return WorkflowPlan(actions=actions)
+                if event_type not in event_trigger_actions:
+                    event_trigger_actions[event_type] = []
+                event_trigger_actions[event_type].append(action.name)
+        return WorkflowPlan(actions=actions, event_trigger_actions=event_trigger_actions)
 
-
-    def get_actions(self, event_type: Type[Event]) -> List[Action]:
+    def get_actions(self, event_type: str) -> List[Action]:
         """Get actions that listen to the specified event type.
 
         Parameters
@@ -63,7 +64,8 @@ class WorkflowPlan(BaseModel):
         list[Action]
             List of Actions that will respond to this event type.
         """
-        return self.actions[event_type]
+        return [self.actions[name] for name in self.event_trigger_actions[event_type]]
+
 
 def _get_actions(workflow: Workflow) -> List[Action]:
     """Extract all registered workflow actions from a workflow.
@@ -82,8 +84,10 @@ def _get_actions(workflow: Workflow) -> List[Action]:
     for name, value in workflow.__class__.__dict__.items():
         if isinstance(value, staticmethod) and hasattr(value, '_listen_events'):
             actions.append(Action(name=name, exec=PythonFunction.from_callable(value.__func__),
-                                  listen_event_types=value._listen_events))
+                                  listen_event_types=[f'{event_type.__module__}.{event_type.__name__}'
+                                                      for event_type in value._listen_events]))
         elif callable(value) and hasattr(value, '_listen_events'):
             actions.append(Action(name=name, exec=PythonFunction.from_callable(value),
-                                  listen_event_types=value._listen_events))
+                                  listen_event_types=[f'{event_type.__module__}.{event_type.__name__}'
+                                                      for event_type in value._listen_events]))
     return actions
