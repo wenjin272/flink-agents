5845ce770b48fabde9f90de8cdfe18945add5b8b
WenjinXie <wenjin272@gmail.com>
2025-08-26 17:15:26 +0800

    [integration][python] Integrate openai chat model.

---
+++ python/flink_agents/integrations/chat_models/openai/tests/test_openai_chat_model.py
@@ -0,0 +1,98 @@
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+import os
+
+import pytest
+
+from flink_agents.api.chat_message import ChatMessage, MessageRole
+from flink_agents.api.resource import Resource, ResourceType
+from flink_agents.integrations.chat_models.openai.openai_chat_model import (
+    OpenAIChatModelConnection,
+    OpenAIChatModelSetup,
+)
+from flink_agents.plan.tools.function_tool import from_callable
+
+test_model = os.environ.get("TEST_MODEL")
+api_key = os.environ.get("TEST_API_KEY")
+api_base_url = os.environ.get("TEST_API_BASE_URL")
+
+
+@pytest.mark.skipif(api_key is None, reason="TEST_API_KEY is not set")
+def test_openai_chat_model() -> None:  # noqa: D103
+    connection = OpenAIChatModelConnection(
+        name="openai", api_key=api_key, api_base_url=api_base_url
+    )
+
+    def get_resource(name: str, type: ResourceType) -> Resource:
+        if type == ResourceType.CHAT_MODEL_CONNECTION:
+            return connection
+        else:
+            return get_resource(name, ResourceType.TOOL)
+
+    chat_model = OpenAIChatModelSetup(
+        name="openai", model=test_model, connection="openai", get_resource=get_resource
+    )
+    response = chat_model.chat([ChatMessage(role=MessageRole.USER, content="Hello!")])
+    assert response is not None
+    assert str(response).strip() != ""
+
+
+def add(a: int, b: int) -> int:
+    """Calculate the sum of a and b.
+
+    Parameters
+    ----------
+    a : int
+        The first operand
+    b : int
+        The second operand
+
+    Returns:
+    -------
+    int:
+        The sum of a and b
+    """
+    return a + b
+
+
+@pytest.mark.skipif(api_key is None, reason="TEST_API_KEY is not set")
+def test_openai_chat_with_tools() -> None:  # noqa : D103
+    connection = OpenAIChatModelConnection(
+        name="openai", api_key=api_key, api_base_url=api_base_url
+    )
+
+    def get_resource(name: str, type: ResourceType) -> Resource:
+        if type == ResourceType.CHAT_MODEL_CONNECTION:
+            return connection
+        else:
+            return from_callable(name=name, func=add)
+
+    chat_model = OpenAIChatModelSetup(
+        name="openai",
+        model=test_model,
+        connection="openai",
+        tools=["add"],
+        get_resource=get_resource,
+    )
+    response = chat_model.chat(
+        [ChatMessage(role=MessageRole.USER, content="What is 377 + 688?")]
+    )
+    tool_calls = response.tool_calls
+    assert len(tool_calls) == 1
+    tool_call = tool_calls[0]
+    assert add(**tool_call["function"]["arguments"]) == 1065
---
+++ python/flink_agents/integrations/chat_models/openai/utils.py
@@ -0,0 +1,131 @@
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+import json
+import os
+from typing import List, Optional, Sequence, Tuple, cast
+
+import openai
+from openai.types.chat import ChatCompletionMessage, ChatCompletionMessageParam
+
+from flink_agents.api.chat_message import ChatMessage, MessageRole
+
+DEFAULT_OPENAI_API_BASE_URL = "https://api.openai.com/v1"
+DEFAULT_OPENAI_API_VERSION = ""
+
+
+def resolve_openai_credentials(
+    api_key: Optional[str] = None,
+    api_base_url: Optional[str] = None,
+    api_version: Optional[str] = None,
+) -> Tuple[Optional[str], str, str]:
+    """Resolve OpenAI credentials.
+
+    The order of precedence is:
+    1. param
+    2. env
+    3. openai module
+    4. default
+    """
+    # resolve from param or env
+    api_key = get_from_param_or_env("api_key", api_key, "OPENAI_API_KEY", "")
+    api_base_url = get_from_param_or_env(
+        "api_base", api_base_url, "OPENAI_API_BASE", ""
+    )
+    api_version = get_from_param_or_env(
+        "api_version", api_version, "OPENAI_API_VERSION", ""
+    )
+
+    # resolve from openai module or default
+    final_api_key = api_key or openai.api_key or ""
+    final_api_base_url = api_base_url or openai.base_url or DEFAULT_OPENAI_API_BASE_URL
+    final_api_version = api_version or openai.api_version or DEFAULT_OPENAI_API_VERSION
+
+    return final_api_key, str(final_api_base_url), final_api_version
+
+
+def get_from_param_or_env(
+    key: str,
+    param: Optional[str] = None,
+    env_key: Optional[str] = None,
+    default: Optional[str] = None,
+) -> str:
+    """Get a value from a param or an environment variable."""
+    if param is not None:
+        return param
+    elif env_key and env_key in os.environ and os.environ[env_key]:
+        return os.environ[env_key]
+    elif default is not None:
+        return default
+    else:
+        msg = (
+            f"Did not find {key}, please add an environment variable"
+            f" `{env_key}` which contains it, or pass"
+            f"  `{key}` as a named parameter."
+        )
+        raise ValueError(msg)
+
+
+def convert_to_openai_messages(
+    messages: Sequence[ChatMessage],
+) -> List[ChatCompletionMessageParam]:
+    """Convert chat messages to OpenAI messages."""
+    return [convert_to_openai_message(message) for message in messages]
+
+
+def convert_to_openai_message(message: ChatMessage) -> ChatCompletionMessageParam:
+    """Convert a chat message to an OpenAI message."""
+    context_txt = message.content
+    context_txt = (
+        None
+        if context_txt == ""
+        and message.role == MessageRole.ASSISTANT
+        and len(message.tool_calls) > 0
+        else context_txt
+    )
+    if len(message.tool_calls) > 0:
+        openai_message = {
+            "role": message.role.value,
+            "content": context_txt,
+            "tool_calls": message.tool_calls,
+        }
+    else:
+        openai_message = {"role": message.role.value, "content": context_txt}
+    openai_message.update(message.extra_args)
+    return cast("ChatCompletionMessageParam", openai_message)
+
+
+def convert_from_openai_message(message: ChatCompletionMessage) -> ChatMessage:
+    """Convert an OpenAI message to a chat message."""
+    tool_calls = []
+    if message.tool_calls:
+        tool_calls = [
+            {
+                "id": tool_call.id,
+                "type": tool_call.type,
+                "function": {
+                    "name": tool_call.function.name,
+                    "arguments": json.loads(tool_call.function.arguments),
+                },
+            }
+            for tool_call in message.tool_calls
+        ]
+    return ChatMessage(
+        role=MessageRole(message.role),
+        content=message.content or "",
+        tool_calls=tool_calls,
+    )
