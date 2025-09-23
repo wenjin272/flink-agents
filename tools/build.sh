e3c0da3fb7e7fe5df0a064ac736c4bb208c62cde
WenjinXie <wenjin272@gmail.com>
2025-09-23 16:39:32 +0800

    [package] Support package flink-agents-dist jar in python wheel.

--- tools/build.sh
+++ tools/build.sh
@@ -15,11 +15,43 @@
 #  limitations under the License.
 #
 
+# Parse command-line arguments
+build_java=true
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -p|--python)
+            build_java=false
+            ;;
+        *)
+            echo "Error: Unknown option '$1'" >&2
+            show_help
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+CURR_DIR=`pwd`
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PROJECT_ROOT="${BASE_DIR}/../"
+
 # build java
-mvn clean package -DskipTests
+if $build_java; then
+    mvn clean package -DskipTests
+fi
+
+# copy flink-agents-dist jar to python lib
+PYTHON_LIB_DIR=${PROJECT_ROOT}/python/flink_agents/lib
+rm -rf ${PYTHON_LIB_DIR}
+mkdir -p ${PYTHON_LIB_DIR}
+
+PROJECT_VERSION=$(sed -n 's/.*<version>\(.*\)<\/version>.*/\1/p' pom.xml | head -n 2 | tail -n 1)
+cp "${PROJECT_ROOT}/dist/target/flink-agents-dist-${PROJECT_VERSION}.jar" ${PYTHON_LIB_DIR}
 
 # build python
 cd python
 uv sync --extra dev
 uv run python -m build
 uv pip install dist/*.whl
+
+rm -rf ${PYTHON_LIB_DIR}
